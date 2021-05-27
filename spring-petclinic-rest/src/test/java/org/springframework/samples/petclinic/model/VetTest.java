9be0677e3043e3c3ef4e1072a805dd76178ab321
wasimeljomaa <77387354+wasimeljomaa@users.noreply.github.com>
2021-05-27 10:05:47 +0200

    update testuser

--- spring-petclinic-rest/src/test/java/org/springframework/samples/petclinic/model/VetTest.java
+++ spring-petclinic-rest/src/test/java/org/springframework/samples/petclinic/model/VetTest.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic.model;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class VetTest {
@@ -29,6 +31,17 @@ public class VetTest {
         Assertions.assertEquals(1, vetTest.getNrOfSpecialties());
         vetTest.clearSpecialties();
         Assertions.assertEquals(0, vetTest.getNrOfSpecialties());
+    }
+    @Test
+    public void testsetSpecialtiesInternal(){
+        Vet vetTest = new Vet();
+        Specialty type1 = new Specialty();
+        type1.setName("Surgery");
+        vetTest.addSpecialty(type1);
+        Set<Specialty> temp = new HashSet<>();
+        temp.add(type1);
+        vetTest.setSpecialtiesInternal(temp);
+        Assertions.assertTrue (vetTest.getSpecialties().contains(type1) );
     }
 
 }
